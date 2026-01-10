de9968308a746ea797f66e0643075d17e5047078
Ahmad <farhadataee2010@gmail.com>
2026-01-09 23:35:08 -0800

    update

---
+++ main.js
@@ -0,0 +1,26 @@
+// nav-menu
+// nav-close
+// nav-toggle
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener("click", ()=>{
+        navMenu.classList.add('show-menu');
+    });
+}
+if(navClose) {
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show-menu');
+    });
+}
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+
+    navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((e) => e.addEventListener('click', linkAction));
